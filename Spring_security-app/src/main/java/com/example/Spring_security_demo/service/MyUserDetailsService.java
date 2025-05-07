166725f971582a8d35403744e76a2eb01879b1d7
Ujjwal Raj <149006191+spartan-0007@users.noreply.github.com>
2025-05-07 18:46:33 +0530

    Base

---
+++ Spring_security-app/src/main/java/com/example/Spring_security_demo/service/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.Spring_security_demo.service;
+
+import com.example.Spring_security_demo.dao.UserRepo;
+import com.example.Spring_security_demo.model.User;
+import com.example.Spring_security_demo.model.UserPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       User user= repo.findByUsername(username);
+       if(user==null){
+           throw new UsernameNotFoundException("User 404");
+       }
+        return new UserPrincipal(user);
+    }
+}
