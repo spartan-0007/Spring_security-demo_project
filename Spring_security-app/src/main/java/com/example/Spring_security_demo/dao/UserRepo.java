ec80632abf61114eb31289f4bf81089792252555
Ujjwal Raj <149006191+spartan-0007@users.noreply.github.com>
2025-05-08 12:50:07 +0530

    Delete Spring_security-app directory

--- Spring_security-app/src/main/java/com/example/Spring_security_demo/dao/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.Spring_security_demo.dao;
-
-import com.example.Spring_security_demo.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User,Integer> {
-    User findByUsername(String username);
-
-}
